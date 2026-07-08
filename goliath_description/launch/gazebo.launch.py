0a3bf3db0d889beea43c9c3ed49eb9fcc706df25
Bart van der Haagen <137152289+bvdhaagen@users.noreply.github.com>
2026-07-08 23:34:54 +0200

    Enhance Gazebo launch with camera support and battery rack
    Added functionality to enable cameras in the robot description and included a new battery rack model in the Gazebo launch file.

--- goliath_description/launch/gazebo.launch.py
+++ goliath_description/launch/gazebo.launch.py
@@ -21,7 +21,8 @@ def generate_launch_description():
                                       ),
                                       description="Absolute path to robot urdf file"
     )
-
+    
+    
     world_name_arg = DeclareLaunchArgument(name="world_name", default_value="datacenter")
 
     world_path = PathJoinSubstitution([
@@ -36,11 +37,28 @@ def generate_launch_description():
 
     env_var = SetEnvironmentVariable("GAZEBO_MODEL_PATH", model_path)
 
-    robot_description = ParameterValue(Command(["xacro ", LaunchConfiguration("model")]),
-                                       value_type=str)
+    robot_description = ParameterValue(
+        Command([
+        "xacro ", 
+        LaunchConfiguration("model"),
+        " enable_cameras:=", LaunchConfiguration("enable_cameras", default="true")
+    ]),
+    value_type=str
+)
+    #======================================================
+    # fixed models to be included in world later 
+    #======================================================
+    
     switch_wall_model = os.path.join(
         goliath_description, "models", "switch_wall", "model.sdf"
     )
+    
+    battery_rack_model = os.path.join(
+        goliath_description, "models", "battery_rack", "model.sdf"
+    )
+        
+    
+    #======================================================
     
     robot_state_publisher_node = Node(
         package="robot_state_publisher",
@@ -53,8 +71,9 @@ def generate_launch_description():
         PythonLaunchDescriptionSource(
             os.path.join(gazebo_ros_dir, "launch", "gzserver.launch.py")
         ),
+        
         launch_arguments={
-            "world": world_path,
+            "world": world_path, "enable_cameras": "True"
         }.items(),
     )
 
@@ -83,85 +102,18 @@ def generate_launch_description():
         output="screen",
     )
     
-    return LaunchDescription([
-        env_var,
-        model_arg,
-        world_name_arg,
-        start_gazebo_server,
-        start_gazebo_client,
-        robot_state_publisher_node,
-        spawn_robot,
-        spawn_wall
-    ])import os
-from os import pathsep
-from ament_index_python.packages import get_package_share_directory, get_package_prefix
-
-from launch import LaunchDescription
-from launch.actions import DeclareLaunchArgument, IncludeLaunchDescription, SetEnvironmentVariable
-from launch.substitutions import Command, LaunchConfiguration, PathJoinSubstitution, PythonExpression
-from launch.launch_description_sources import PythonLaunchDescriptionSource
-
-from launch_ros.actions import Node
-from launch_ros.parameter_descriptions import ParameterValue
-
-
-def generate_launch_description():
-    goliath_description = get_package_share_directory("goliath_description")
-    goliath_description_prefix = get_package_prefix("goliath_description")
-    gazebo_ros_dir = get_package_share_directory("gazebo_ros")
-
-    model_arg = DeclareLaunchArgument(name="model", default_value=os.path.join(
-                                      goliath_description, "urdf", "goliath.urdf.xacro"
-                                      ),
-                                      description="Absolute path to robot urdf file"
+    spawn_rack = Node(
+        package="gazebo_ros",
+        executable="spawn_entity.py",
+        arguments=[
+            "-entity", "battery_rack",
+            "-file", battery_rack_model,
+            "-x", "9.0", "-y", "4.5", "-z", "0.0",
+        ],
+        output="screen",
     )
-
-    world_name_arg = DeclareLaunchArgument(name="world_name", default_value="datacenter")
-
-    world_path = PathJoinSubstitution([
-            goliath_description,
-            "worlds",
-            PythonExpression(expression=["'", LaunchConfiguration("world_name"), "'", " + '.world'"])
-        ]
-    )
-
-    model_path = os.path.join(goliath_description, "models")
-    model_path += pathsep + os.path.join(goliath_description_prefix, "share")
-
-    env_var = SetEnvironmentVariable("GAZEBO_MODEL_PATH", model_path)
-
-    robot_description = ParameterValue(Command(["xacro ", LaunchConfiguration("model")]),
-                                       value_type=str)
-
-    robot_state_publisher_node = Node(
-        package="robot_state_publisher",
-        executable="robot_state_publisher",
-        parameters=[{"robot_description": robot_description,
-                     "use_sim_time": True}]
-    )
-
-    start_gazebo_server = IncludeLaunchDescription(
-        PythonLaunchDescriptionSource(
-            os.path.join(gazebo_ros_dir, "launch", "gzserver.launch.py")
-        ),
-        launch_arguments={
-            "world": world_path,
-        }.items(),
-    )
-
-    start_gazebo_client = IncludeLaunchDescription(
-        PythonLaunchDescriptionSource(
-            os.path.join(gazebo_ros_dir, "launch", "gzclient.launch.py")
-        )
-    )
-
-    spawn_robot = Node(package="gazebo_ros", executable="spawn_entity.py",
-                        arguments=["-entity", "goliath",
-                                   "-topic", "robot_description",
-                                  ],
-                        output="screen"
-    )
-
+    
+    
     return LaunchDescription([
         env_var,
         model_arg,
@@ -170,4 +122,6 @@ def generate_launch_description():
         start_gazebo_client,
         robot_state_publisher_node,
         spawn_robot
+        #spawn_rack,
+        #spawn_wall
     ])
